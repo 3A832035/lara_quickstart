4947659f29efec159348a86669a418503fb2c303
3A832035 <3a832035@gm.student.ncut.edu.tw>
2021-12-12 22:16:11 +0800

    建置路由&顯示視圖

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,23 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+/**
+ * 顯示所有任務
+ */
 Route::get('/', function () {
-    return view('welcome');
+    return view('tasks');
+});
+
+/**
+ * 增加新的任務
+ */
+Route::post('/task', function (Request $request) {
+    //
+});
+
+/**
+ * 刪除任務
+ */
+Route::delete('/task/{task}', function (Task $task) {
+    //
 });
